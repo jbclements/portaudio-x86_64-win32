a0374076ff36f87e49b7b122fbce80c4118ec222
John Clements <clements@racket-lang.org>
2022-09-01 20:19:58 -0400

    update to new ffi mechanism

--- info.rkt
+++ info.rkt
@@ -1,7 +1,9 @@
 #lang setup/infotab
 
-(define collection 'multi)
 (define version "0.1")
-
+(define deps '("base"))
+(define copy-foreign-libs '("portaudio.dll"
+                            "librsoundcallbacks.dll"))
+(define install-platform "win32\\x86_64")
 
 
