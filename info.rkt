9c30ff74dd26275b9e99776d1fcda09cc714c123
John Clements <clements@racket-lang.org>
2024-03-02 12:50:02 -0800

    update info.rkt to reflect library name change oops

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define version "0.1")
 (define deps '("base"))
-(define copy-foreign-libs '("portaudio.dll"
+(define copy-foreign-libs '("libportaudio.dll"
                             "librsoundcallbacks.dll"))
 (define install-platform "win32\\x86_64")
 
